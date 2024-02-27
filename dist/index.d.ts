de40e2766038647d279c17f5dbd50ea14f8ee27d
Daniil Makeev <makeev@satel.org>
2024-02-27 21:03:15 +0300

    Debug

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,1 @@
-export { P2PCallController as P2PEngine, P2PCallEventType } from './controllers/p2p-call';
-export type { User as P2PUser, P2PCall, UserPairInfo, UniError } from './models/index';
-export type { P2PCallEvent } from './controllers/p2p-call';
+export * from './index';
--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,16 @@
+import * as WebRTC from 'react-native-webrtc';
+import { injectWebRTC } from './controllers/webrtc';
+console.log('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@');
+console.log('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@');
+console.log('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@');
+console.log('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@');
+console.log('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@');
+console.log('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@');
+injectWebRTC(WebRTC);
+export * from './index';
+/*
 export { P2PCallController as P2PEngine, P2PCallEventType } from './controllers/p2p-call';
+export type { User as P2PUser, P2PCall, UserPairInfo, UniError } from './models/index';
+export type { P2PCallEvent } from './controllers/p2p-call';
+*/
 //# sourceMappingURL=index.js.map
