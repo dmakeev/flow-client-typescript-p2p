6449084dbaa2b21c703d8b4b5bb949332f1aa384
Daniil Makeev <makeev@satel.org>
2024-02-27 21:06:57 +0300

    Debug

--- src/index.ts
+++ src/index.ts
@@ -8,10 +8,6 @@ console.log('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@'
 console.log('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@');
 injectWebRTC(WebRTC);
 
-export * from './index';
-
-/*
 export { P2PCallController as P2PEngine, P2PCallEventType } from './controllers/p2p-call';
 export type { User as P2PUser, P2PCall, UserPairInfo, UniError } from './models/index';
 export type { P2PCallEvent } from './controllers/p2p-call';
-*/
