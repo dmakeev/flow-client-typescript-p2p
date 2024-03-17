152e75a3132c7714d8919a023579950dd82585f3
Daniil Makeev <makeev@satel.org>
2024-03-17 21:54:13 +0300

    Debug

--- dist/controllers/webrtc.js
+++ dist/controllers/webrtc.js
@@ -92,14 +92,22 @@ export class WebRTCController {
                     const currentAudioTrack = this.localStream?.getAudioTracks().length ? this.localStream?.getAudioTracks()[0] : null;
                     const newAudioTrack = stream.getAudioTracks().length ? this.localStream?.getAudioTracks()[0] : null;
                     this.connection?.getSenders().forEach((sender) => {
+                        console.log('$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ +++  1');
                         if (!!currentVideoTrack && !!newVideoTrack && sender.track?.id === currentVideoTrack.id) {
+                            console.log('$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ +++  2');
                             sender.replaceTrack(newVideoTrack);
                         }
                         if (!!currentAudioTrack && !!newAudioTrack && sender.track?.id === currentAudioTrack.id) {
+                            console.log('$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ +++  3');
                             sender.replaceTrack(newAudioTrack);
                         }
                     });
                     this.localStream = stream;
+                    this.eventListeners.get(WebRTCEventType.LOCAL_STREAM)?.forEach((listener) => {
+                        // this.videoStream = new WebRTC.MediaStream(this.localStream?.getVideoTracks());
+                        // listener({ stream: this.videoStream });
+                        listener({ stream });
+                    });
                     currentVideoTrack?.stop();
                     newAudioTrack?.stop();
                     resolve();
