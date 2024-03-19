56d934b3b13c8e52a136bf2e7dfce88c4aedbf2b
Daniil Makeev <makeev@satel.org>
2024-03-19 20:25:06 +0300

    Debug

--- src/controllers/webrtc.ts
+++ src/controllers/webrtc.ts
@@ -106,9 +106,11 @@ export class WebRTCController {
                         const newVideoTrack = stream.getVideoTracks().length ? stream?.getVideoTracks()[0] : null;
                         if (!!newAudioTrack) {
                             newAudioTrack!.enabled = currentAudioTrack?.enabled ?? false;
+                            console.log('!!!!!! A', newAudioTrack!.enabled);
                         }
                         if (!!newVideoTrack) {
                             newVideoTrack!.enabled = currentVideoTrack?.enabled ?? false;
+                            console.log('!!!!!! B', newAudioTrack!.enabled);
                         }
                         if (!!this.connection) {
                             for (let sender of this.connection.getSenders()) {
